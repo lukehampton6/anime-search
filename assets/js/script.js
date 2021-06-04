e8e813ba0f0b37fb501bb9a7517f81f2461abfa1
mtpullin <mtpullin@gmail.com>
2021-06-04 14:09:51 -0400

    added more comments to try to make it easier to understand

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,11 +17,14 @@ var animeNameSearch = function(event) {
 }
 //display anime function will be for appending anime titles/pictures to new anime list div in html
 var displayAnime = function(animesearch) {
+    //text content should reset to blank after enter button is clicked and anime is displayed
     userInput.textContent=""
+    //if anime isn't able to display it should return from function and say anime not found in div
     if(animesearch.length === 0) {
         animeList.textContent = "no anime found"
         return;
     }
+    //beginning of for loop to list anime on page
     for(var i = 0; i < animesearch.length; i++) {
 
     }
