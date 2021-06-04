9a2f6154eaeed632fbb7648c15dfaeb1f9a8c2ca
mtpullin <mtpullin@gmail.com>
2021-06-04 09:31:39 -0400

    added div for anime list as well as some initial JS for functions

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+var enterBtn = document.querySelector(".button")
+var userInput = document.querySelector(".input")
+var apiUrl = "https://api.jikan.moe/v3/search/anime?q=naruto";
+var animeName = userInput.value.trim();
+var animeList = document.querySelector("#animeList")
+
+var animeNameSearch = function(event) {
+    
+    if(animeName) {
+        getAnime(animeName);
+        userInput.value = "";
+    } else {
+        return;
+    }
+}
+var displayAnime = function(animesearch) {
+    userInput.textContent=""
+    if(animesearch.length === 0) {
+        animeList.textContent = "no anime found"
+        return;
+    }
+    for(var i = 0; i < animesearch.length; i++) {
+
+    }
+
+
+}
+var getAnime = function(anime) {
+    fetch(apiUrl)
+    .then(function(response){
+        if(response.ok) {
+            response.json().then(function(data){
+                displayAnime(data, anime)
+            })
+        }
+    })
+}
+enterBtn.addEventListener("submit", animeNameSearch())
