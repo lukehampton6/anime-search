75ebfdfc8f30a645f1c4407c112ebbe3093aedfe
mtpullin <mtpullin@gmail.com>
2021-06-04 14:06:57 -0400

    added comments for functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ var apiUrl = "https://api.jikan.moe/v3/search/anime?q=naruto";
 var animeName = userInput.value.trim();
 var animeList = document.querySelector("#animeList")
 
+
+//anime seach function will pull user info to get anime name
 var animeNameSearch = function(event) {
     
     if(animeName) {
@@ -13,6 +15,7 @@ var animeNameSearch = function(event) {
         return;
     }
 }
+//display anime function will be for appending anime titles/pictures to new anime list div in html
 var displayAnime = function(animesearch) {
     userInput.textContent=""
     if(animesearch.length === 0) {
@@ -25,6 +28,8 @@ var displayAnime = function(animesearch) {
 
 
 }
+
+//get anime function will fetch the anime names/pictures from the api
 var getAnime = function(anime) {
     fetch(apiUrl)
     .then(function(response){
